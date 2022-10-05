d9c95c86072667cfae7356a9416f9d5a69e7454b
kml htb <kmlhtbdev@gmail.com>
2022-10-05 14:31:02 +0300

    Initial commit

---
+++ src/havaDurumu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class havaDurumu {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Sıcaklık Derecesini Giriniz: ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.print("Kayağa Gidebilirsiniz");
+        }else if (heat >=5 && heat <15){
+            System.out.print("Sinemaya Gidebilirsiniz");
+        } else if (heat >=15 && heat <= 25){
+            System.out.print("Pikniğe Gidebilirsiniz");
+        } else {
+            System.out.print("Yüzmeye Gidebilirsiniz");
+        }
+    }
+}
